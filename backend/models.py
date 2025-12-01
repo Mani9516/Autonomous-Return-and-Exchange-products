6d0e318e4ccef0ced1f2518cd1e1a51ef60cee0b
Mani Chourasiya <97172597+Mani9516@users.noreply.github.com>
2025-12-01 11:34:22 +0530

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from typing import Optional, List
+from datetime import datetime
+from sqlmodel import Field, SQLModel, Relationship
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(unique=True)
+    password_hash: str
+    orders: List["Order"] = Relationship(back_populates="user")
+
+class Order(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    status: str
+    user: Optional[User] = Relationship(back_populates="orders")
